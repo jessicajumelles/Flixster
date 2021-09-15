a63ead76ead97a51aaf7f7a3ab617988f26868e6
Jessica Jumelles <jessicajumelles@gmail.com>
2021-09-15 18:32:24 -0400

    Updated the Flixster to open another activity that open the movie description as well as Youtube trailer of that movie.

--- app/src/main/java/com/codepath/flixster/adapters/MovieAdapters.java
+++ app/src/main/java/com/codepath/flixster/adapters/MovieAdapters.java
@@ -1,22 +1,31 @@
 package com.codepath.flixster.adapters;
 
+import androidx.core.app.ActivityOptionsCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.media.VolumeShaper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 
 import com.bumptech.glide.Glide;
+import com.codepath.flixster.DetailActivity;
+import com.codepath.flixster.MainActivity;
 import com.codepath.flixster.R;
 import com.codepath.flixster.models.Movie;
 import android.util.Log;
+import android.widget.Toast;
+
+import org.parceler.Parcels;
+
 import java.util.List;
 
 public class MovieAdapters extends RecyclerView.Adapter<MovieAdapters.ViewHolder> {
@@ -52,6 +61,8 @@ public class MovieAdapters extends RecyclerView.Adapter<MovieAdapters.ViewHolder
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+
+        RelativeLayout container;
         TextView tvTitle;
         TextView tvOverview;
         ImageView ivPoster;
@@ -61,6 +72,7 @@ public class MovieAdapters extends RecyclerView.Adapter<MovieAdapters.ViewHolder
             tvTitle = itemView.findViewById(R.id.tvTitle);
             tvOverview = itemView.findViewById(R.id.tvOverview);
             ivPoster = itemView.findViewById(R.id.ivPoster);
+            container = itemView.findViewById(R.id.container);
         }
 
         public void bind(Movie movie) {
@@ -78,6 +90,18 @@ public class MovieAdapters extends RecyclerView.Adapter<MovieAdapters.ViewHolder
             }
 
             Glide.with(context).load(imageUrl).into(ivPoster);
+
+            //1. register the click listen on the whole container
+            container.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //2. the action taken when we tap to navigate to a new activity
+                   // Toast.makeText(context, movie.getTitle(), Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(context, DetailActivity.class);
+                    i.putExtra("movie", Parcels.wrap(movie));
+                    context.startActivity(i);
+                }
+            });
 
         }
     }
